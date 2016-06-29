27e0acd13a72fe5acda915f180dec224d72013e1
Yoriki Yamaguchi <yamaguchi@cloudpack.jp>
2016-06-29 14:28:13 +0900

    add ENV["AWS_DEFAULT_REGION"]

--- itamae/cookbooks/base/files/etc/profile.d/_ec2_env.sh
+++ itamae/cookbooks/base/files/etc/profile.d/_ec2_env.sh
@@ -1,6 +1,7 @@
 export INSTANCE_IDENTITY_DOCUMENT=`curl -s 169.254.169.254/latest/dynamic/instance-identity/document`
 export AWS_ACCOUNT_ID=`echo $INSTANCE_IDENTITY_DOCUMENT | jq -r ".accountId"`
 export AWS_REGION=`echo $INSTANCE_IDENTITY_DOCUMENT | jq -r ".region"`
+export AWS_DEFAULT_REGION=$AWS_REGION
 export AVAILABILITY_ZONE=`echo $INSTANCE_IDENTITY_DOCUMENT | jq -r ".availabilityZone"`
 export INSTANCE_ID=`echo $INSTANCE_IDENTITY_DOCUMENT | jq -r ".instanceId"`
 export INSTANCE_TYPE=`echo $INSTANCE_IDENTITY_DOCUMENT | jq -r ".instanceType"`
@@ -8,7 +9,7 @@ export IMAGE_ID=`echo $INSTANCE_IDENTITY_DOCUMENT | jq -r ".imageId"`
 
 export PUBLIC_HOSTNAME=`curl -s 169.254.169.254/latest/meta-data/public-hostname | head -n 1 | grep -e "^[^<]"`
 
-_tagged_name=$(aws ec2 describe-instances --region $AWS_REGION --instance-ids $INSTANCE_ID --query 'Reservations[].Instances[].[Tags[?Key==`Name`].Value]' --output text 2>/dev/null)
+_tagged_name=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[].Instances[].[Tags[?Key==`Name`].Value]' --output text 2>/dev/null)
 
 if [ $? -eq 0 ]; then
   export TAGGED_NAME=$_tagged_name
