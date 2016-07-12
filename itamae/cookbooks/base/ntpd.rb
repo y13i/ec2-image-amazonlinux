51a67951c997b736de29a5e88f9fe95f7f45cbb6
Hiroyuki Sato <sato@cloudpack.jp>
2016-07-12 16:19:04 +0900

    #1 Enable ntpd slew mode

--- itamae/cookbooks/base/ntpd.rb
+++ itamae/cookbooks/base/ntpd.rb
@@ -1,9 +1,7 @@
-template "/etc/sysconfig/ntpd" do
+file "/etc/sysconfig/ntpd" do
   action :create
   user   "root"
   owner  "root"
   group  "root"
   mode   "0644"
-  source :auto
-  notifies :restart, "service[ntpd]"
 end
