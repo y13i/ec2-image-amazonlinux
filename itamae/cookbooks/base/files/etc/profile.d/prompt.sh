82b06dc53009ab706ca25b1706e28b8d433e3076
Yoriki Yamaguchi <yamaguchi@cloudpack.jp>
2016-08-17 21:02:23 +0900

    use promt letter "#" if user is root

--- itamae/cookbooks/base/files/etc/profile.d/prompt.sh
+++ itamae/cookbooks/base/files/etc/profile.d/prompt.sh
@@ -1,5 +1,11 @@
-if [ -n "$TAGGED_NAME" ]; then
-  export PS1="[\u@\h (\[\e[1m\]$INSTANCE_ID\[\e[m\], \[\e[1m\]$TAGGED_NAME\[\e[m\]) \W]\$ "
+if [ "$USER" = "root" ]; then
+  pr="#"
 else
-  export PS1="[\u@\h (\[\e[1m\]$INSTANCE_ID\[\e[m\]) \W]\$ "
+  pr="$"
+fi
+
+if [ -n "$TAGGED_NAME" ]; then
+  export PS1="[\u@\h (\[\e[1m\]$INSTANCE_ID\[\e[m\], \[\e[1m\]$TAGGED_NAME\[\e[m\]) \W]$pr "
+else
+  export PS1="[\u@\h (\[\e[1m\]$INSTANCE_ID\[\e[m\]) \W]$pr "
 fi
